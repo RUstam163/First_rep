b1fd5e97e1346893e7e6e5dee904b3582e0bc383
RUstam163 <minibaev-rustam163@yandex.ru>
2019-04-02 16:24:14 +0400

    first_commit_JQwery_test

--- script.js
+++ script.js
@@ -51,4 +51,76 @@ let stores = [
         ]
 
     }
-]
+];
+
+function selectMas(mas){
+    for (let i = 0; i < mas.length; i++) {        
+        $('select#shopName').append($(new Option(mas[i].name, value=i))); 
+    }
+}
+
+$( "#shopName" ).change(function() {
+    var shop = stores[$("#shopName").val()]
+    var tbl = '<table border=1 id="table">';
+    tbl += "<tr><th>id</th><th>Товар</th><th>Цвет</th><th>Цена</th><th>Изменить</th><th>Удалить</th></tr>";
+    
+    for (let i = 0; i < shop.goods.length; i++) {
+        var goods = shop.goods[i];
+  
+            tbl += '<tr class="tr">';
+            tbl += '<td class="g_id" >' + goods.id + "</td>";
+            tbl += '<td class="g_name">' + goods.name + "</td>";
+            tbl += '<td class="g_color">' + goods.color + "</td>";
+            tbl += '<td class="g_price">' + goods.price + "</td>";
+            tbl += "<td>" + '<input type="button" class="edit" value="Изменить">' + "</td>";
+            tbl += "<td>" + '<input type="button" class="del" value = "Удалить">' + "</td>";
+            tbl += "</tr>";   
+                             
+    }   
+    
+        tbl += "</table>";
+        $("#table").html(tbl);
+
+    $('.edit').on('click', function() {
+        let trCollect = $(this).parent().parent().children('td');
+            var form = '<form class="form">';
+            form +='<input type="hidden" class="id_goods">'
+            form += "<label>"+"Наиминование Товара"+'<input type="text" class="name_goods">' +"</label>"+"<br>";
+            form += "<label>"+"Цвет Товара"+'<input type="text" class="color_goods">' +"</label>"+"<br>";
+            form += "<label>"+"Цена Товара"+'<input type="text" class="price_goods">' +"</label>"+"<br>";
+            form += '<input type="button" class="saveBut" value="Сохранить">';
+            form += '</form>';
+            $("#form").html(form); 
+            $('.saveBut').attr('data-ID',trCollect[0].innerHTML);
+            $('.name_goods').val(trCollect[1].innerHTML);
+            $('.color_goods').val(trCollect[2].innerHTML);
+            $('.price_goods').val(trCollect[3].innerHTML);          
+            
+        $('.saveBut').on('click', function() { 
+            
+            console.log($('tr[2]'));
+            
+            // console.log($('#' + $('.saveBut').attr('data-ID')));
+           
+
+            // console.log($('.g_id').attr("id"))
+            // if ($('.id_goods').val()==$('.g_id').attr("id")) {
+            //     $('.g_name').text($('.name_goods').val());
+            //     $('.g_color').text($('.color_goods').val());
+            //     $('.g_price').text($('.price_goods').val());
+            // }
+            // else alert("AAAAAAAAAAAAAAAAAAAAAAAAaa")
+            // $('.g_name').text($('.name_goods').val());
+            // $('.g_color').text($('.color_goods').val());
+            // $('.g_price').text($('.price_goods').val());
+        });
+
+    });
+                      
+    $('.del').on('click', function() {
+        $(this).closest('tr').remove();
+    });
+});
+
+selectMas(stores)
+
